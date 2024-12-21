d16b7fc595217eed2907b65ab4d747b004282600
RealStateBaba <edigitalbaba7587@gmail.com>
2024-12-21 12:26:12 +0530

    add link ain header

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import HomePage from './pages/Home';
 import CareerPage from './pages/CareerPage';
+import ContactPage from './pages/Contect';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
    <Routes>
     <Route exact path="/" element={<HomePage/>}/>
     <Route exact path="/careers" element={<CareerPage/>}/>
+    <Route exact path="/contact" element={<ContactPage/>}/>
     
    </Routes>
    </Router>
--- src/component/Header.js
+++ src/component/Header.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import PrimaryHeader from "./PrimaryHeader";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -38,43 +39,43 @@ const Header = () => {
           <img src="assets/img/logo-final.png" width={50}/>
 
             <h1>
-              <a href="#body" className="scrollto">
+              <Link href="#body" className="scrollto">
                 First<span>miner</span>
-              </a>
+              </Link>
             </h1>
           </div>
 
           <nav id="nav-menu-container" className={isMenuOpen ? "open" : ""}>
             <ul className="nav-menu">
               <li className="menu-active">
-                <a href="#body">Home</a>
+                <Link href="/">Home</Link>
               </li>
               <li>
-                <a href="/careers">Careers</a>
+                <Link href="/careers">Careers</Link>
               </li>
               <li>
-                <a href="#about">About Us</a>
+                <Link href="#about">About Us</Link>
               </li>
               <li>
-                <a href="#services">Services</a>
+                <Link href="#services">Services</Link>
               </li>
               <li>
-                <a href="#portfolio">Portfolio</a>
+                <Link href="#portfolio">Portfolio</Link>
               </li>
               <li>
-                <a href="#team">Team</a>
+                <Link href="#team">Team</Link>
               </li>
               <li className="menu-has-children">
-                <a href="#">Drop Down</a>
+                <Link href="#">Drop Down</Link>
                 <ul>
-                  <li><a href="#">Drop Down 1</a></li>
-                  <li><a href="#">Drop Down 2</a></li>
-                  <li><a href="#">Drop Down 3</a></li>
-                  <li><a href="#">Drop Down 4</a></li>
+                  <li><Link href="#">Drop Down 1</Link></li>
+                  <li><Link href="#">Drop Down 2</Link></li>
+                  <li><Link href="#">Drop Down 3</Link></li>
+                  <li><Link href="#">Drop Down 4</Link></li>
                 </ul>
               </li>
               <li>
-                <a href="#contact">Contact</a>
+                <Link href="/contact">Contact</Link>
               </li>
             </ul>
           </nav>
