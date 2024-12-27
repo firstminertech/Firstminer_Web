03dee6f4c30f3964fe6494b7e2be3eb35b8db5e9
Shivamgupta0272 <shivamgupta6176@gmail.com>
2024-12-27 15:09:17 +0530

    ui enhance in client

--- src/api/config.js
+++ src/api/config.js
@@ -1,9 +1,9 @@
 
-export const BASE_API_URL = "http://localhost:5000/api/" //TEST
-export const BASE_FILE_URL = "http://localhost:5000/" //TEST
+// export const BASE_API_URL = "http://localhost:5000/api/" //TEST
+// export const BASE_FILE_URL = "http://localhost:5000/" //TEST
 
 //  export const BASE_API_URL = "http://192.168.29.49:8080/api/"  //TEST
 //  export const BASE_FILE_URL = "http://192.168.29.49:8080/" //TEST
 
-// export const BASE_API_URL = "http://192.168.29.120:5000/api/"  //TEST
-// export const BASE_FILE_URL = "http://192.168.29.120:5000/" //TEST
+export const BASE_API_URL = "http://192.168.29.120:5000/api/"  //TEST
+export const BASE_FILE_URL = "http://192.168.29.120:5000/" //TEST
